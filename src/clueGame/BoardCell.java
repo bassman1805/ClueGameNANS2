bb30835381d2a9fba7533265086f086a33e635be
Maddie Rogers <madisonrogers@mymail.mines.edu>
2017-02-28 11:36:36 -0700

    Added the clueGame package

---
+++ src/clueGame/BoardCell.java
@@ -0,0 +1,32 @@
+package clueGame;
+
+public class BoardCell {
+	
+	private int row = 0;
+	private int col = 0;
+
+	public BoardCell() {
+		// TODO Auto-generated constructor stub
+	}
+	
+
+	public BoardCell(int row, int col) {
+		super();
+		this.row =row;
+		this.col = col;
+		
+	}
+
+
+	public int getRow() {
+		return row;
+	}
+
+
+	public int getCol() {
+		return col;
+	}
+	
+	
+
+}
